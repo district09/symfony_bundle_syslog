22775b5c976d3521eb68915fb9fc59571ae88faa
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-11-28 14:04:51 +0100

    Completed BaseUrlProcessorTest.

--- Tests/Monolog/Processor/BaseUrlProcessorTest.php
+++ Tests/Monolog/Processor/BaseUrlProcessorTest.php
@@ -4,6 +4,7 @@ namespace DigipolisGent\SyslogBundle\Tests\Monolog\Processor;
 
 use DigipolisGent\SyslogBundle\Monolog\Processor\BaseUrlProcessor;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class BaseUrlProcessorTest extends TestCase {
@@ -19,4 +20,17 @@ class BaseUrlProcessorTest extends TestCase {
         $this->assertEquals($record['id'], $id);
     }
 
+    public function testInvoke()
+    {
+        $url = uniqid();
+        $request = $this->getMockBuilder(Request::class)->disableOriginalConstructor()->getMock();
+        $request->expects($this->once())->method('getSchemeAndHttpHost')->willReturn($url);
+        $requestStack = $this->getMockBuilder(RequestStack::class)->disableOriginalConstructor()->getMock();
+        $requestStack->expects($this->once())->method('getCurrentRequest')->willReturn($request);
+        $processor = new BaseUrlProcessor($requestStack);
+        $id = uniqid();
+        $record = $processor(['id' => $id]);
+        $this->assertEquals($record['extra']['base_url'], $url);
+        $this->assertEquals($record['id'], $id);
+    }
 }
