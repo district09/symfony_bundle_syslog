26e1580479d68ca878ce6ba6bd9bc8d24d2da647
Peter Decuyper <peter.decuyper@district09.gent>
2024-12-02 16:26:36 +0100

    Fix error about not defined parameter

--- src/DigipolisGentSyslogBundle.php
+++ src/DigipolisGentSyslogBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DigipolisGent\SyslogBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -18,36 +19,26 @@ final class DigipolisGentSyslogBundle extends AbstractBundle
     /**
      * @inheritDoc
      *
-     * Adds the bundle services definitions.
+     * - Adds the bundle services definitions.
+     * - Add a preconfigured Syslog handler using the kibana formatter.
      */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yml');
-    }
-
-    /**
-     * @inheritDoc
-     *
-     * Adds a pre-defined syslog handler to the monolog configuration.
-     */
-    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void {
-        if (!$builder->hasExtension('monolog')) {
-            return;
-        }
 
         $config = [
-            'handlers' => [
-                'syslog' => [
-                    'type' => 'syslog',
-                    'level' => $builder->getParameter('kernel.environment') === 'prod' ? 'notice' : 'warning',
-                    'facility' => defined('LOG_LOCAL4') ? LOG_LOCAL4 : 160,
-                    'ident' => $builder->getParameter('digipolis_syslog_identity') ?: 'no_syslog_identity_set',
-                    'logopts' => LOG_ODELAY,
-                    'formatter' => 'monolog.formatter.kibana',
-                ],
+          'handlers' => [
+            'syslog' => [
+              'type' => 'syslog',
+              'level' => $builder->getParameter('kernel.environment') === 'prod' ? 'notice' : 'warning',
+              'facility' => defined('LOG_LOCAL4') ? LOG_LOCAL4 : 160,
+              'ident' => $builder->hasParameter('digipolis_syslog_identity') ? $builder->getParameter('digipolis_syslog_identity') : 'no_syslog_identity_set',
+              'logopts' => LOG_ODELAY,
+              'formatter' => 'monolog.formatter.kibana',
             ],
+          ],
         ];
-
         $builder->prependExtensionConfig('monolog', $config);
     }
+
 }
